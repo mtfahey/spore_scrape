16b0b7ef58c7c03266f72f92db54963d315a68c4
mtfahey <mtfahey@gmail.com>
2021-05-09 13:36:36 -0400

    added right etl file

--- etl/spore_scrape.py
+++ etl/spore_scrape.py
@@ -7,8 +7,6 @@ example = "http://static.spore.com/static/image/501/091/312/501091312194_lrg.png
 root = "http://static.spore.com/static/image/"
 
 #cycle through the 12-digit id starting at 5 billion
-#500000000000
-#502000000000
 for this_id in range(500000000000, 502000000000):
 	this_id = str(this_id)
 
@@ -18,12 +16,12 @@ for this_id in range(500000000000, 502000000000):
 	dir_3 = this_id[6:9]
 	#make full link
 	link = root+dir_1+"/"+dir_2+"/"+dir_3+"/"+this_id+"_lrg.png"
-
 	print(link)
 
-	#get images
-	#../data/processed/"+
+	#get images and save if not a 404
 	image = requests.get(link)
-	file = open(this_id+"_lrg.png", "wb")
-	file.write(image.content)
-	file.close()
+	if image.status_code != 404:
+		print("saved")
+		file = open("../data/processed/"+this_id+"_lrg.png", "wb")
+		file.write(image.content)
+		file.close()
